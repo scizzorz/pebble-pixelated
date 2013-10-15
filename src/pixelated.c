62bf01940e0a84d4f1b64964658f3c377e322618
John Weachock <jweachock@gmail.com>
2013-10-15 12:41:20 -0400

    Add date words

--- src/pixelated.c
+++ src/pixelated.c
@@ -6,10 +6,20 @@
 
 char* hours[] = {"twelve", "one", "two", "three", "four", "five", "six",
 	"seven", "eight", "nine", "ten", "eleven"};
-char* ones[] = {"oh", "one", "two", "three", "four", "five", "six", "seven", "eight",
-	"nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+char* ones[] = {"oh", "one", "two", "three", "four", "five", "six", "seven",
+	"eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
 	"sixteen", "seventeen", "eighteen", "nineteen"};
 char* tens[] = {"o'clock", "ten", "twenty", "thirty", "forty", "fifty"};
+char* ord_ones[] = {"zeroth", "first", "second", "third", "fourth", "fifth",
+	"sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth",
+	"thirteenth", "fourteenth", "fifteenth", "sixteenth", "seventeenth",
+	"eighteenth", "nineteenth"};
+char* ord_tens[] = {"zeroth", "tenth", "twentieth", "thirtieth"};
+
+char* days[] = {"sunday", "monday", "tuesday", "wednesday", "thursday",
+	"friday", "saturday"};
+char* months[] = {"january", "february", "march", "april", "may", "june", "july",
+	"august", "september", "october", "november", "december"};
 
 PBL_APP_INFO(UUID,
 		"Pixelated", "John Weachock",
@@ -18,8 +28,8 @@ PBL_APP_INFO(UUID,
 		APP_INFO_WATCH_FACE);
 
 Window window;
-TextLayer hour_layer, min_layer, day_layer, date_layer;
-char hour_txt[32], min_txt[32], day_txt[16], date_txt[16];
+TextLayer hour_layer, min_layer, day_layer, month_layer, date_layer;
+char hour_txt[16], min_txt[16], day_txt[16], month_txt[16], date_txt[16];
 
 void min_to_str(int x, char* buffer, int size) {
 	if(x % 10 == 0) {
@@ -33,18 +43,32 @@ void min_to_str(int x, char* buffer, int size) {
 	}
 }
 
+void date_to_str(int x, char* buffer, int size) {
+	if(x % 10 == 0) {
+		snprintf(buffer, size, "%s", ord_tens[x / 10]);
+	} else if(x < 20) {
+		snprintf(buffer, size, "%s", ord_ones[x]);
+	} else {
+		snprintf(buffer, size, "%s %s", ord_tens[x / 10], ord_ones[x % 10]);
+	}
+}
+
 void tick() {
 	PblTm current_time;
 
 	get_time(&current_time);
 
-	snprintf(hour_txt, 32, "%s",
-		hours[current_time.tm_hour % 12]);
-
-	min_to_str(current_time.tm_min, min_txt, 32);
+	snprintf(hour_txt, 16, "%s", hours[current_time.tm_hour % 12]);
+	min_to_str(current_time.tm_min, min_txt, 16);
+	snprintf(day_txt, 16, "%s", days[current_time.tm_wday]);
+	snprintf(month_txt, 16, "%s", months[current_time.tm_mon]);
+	date_to_str(current_time.tm_mday, date_txt, 16);
 
 	text_layer_set_text(&hour_layer, hour_txt);
 	text_layer_set_text(&min_layer, min_txt);
+	text_layer_set_text(&day_layer, day_txt);
+	text_layer_set_text(&month_layer, month_txt);
+	text_layer_set_text(&date_layer, date_txt);
 }
 
 void init(AppContextRef ctx) {
@@ -59,8 +83,23 @@ void init(AppContextRef ctx) {
 	text_layer_set_text_alignment(&min_layer, GTextAlignmentCenter);
 	text_layer_set_font(&min_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
 
+	text_layer_init(&day_layer, GRect(0, 64, 144, 30));
+	text_layer_set_text_alignment(&day_layer, GTextAlignmentCenter);
+	text_layer_set_font(&day_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+
+	text_layer_init(&month_layer, GRect(0, 96, 144, 30));
+	text_layer_set_text_alignment(&month_layer, GTextAlignmentCenter);
+	text_layer_set_font(&month_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+
+	text_layer_init(&date_layer, GRect(0, 128, 144, 30));
+	text_layer_set_text_alignment(&date_layer, GTextAlignmentCenter);
+	text_layer_set_font(&date_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+
 	layer_add_child(&window.layer, &hour_layer.layer);
 	layer_add_child(&window.layer, &min_layer.layer);
+	layer_add_child(&window.layer, &day_layer.layer);
+	layer_add_child(&window.layer, &month_layer.layer);
+	layer_add_child(&window.layer, &date_layer.layer);
 
 	tick();
 }
